162f0b7430b86a2e997902612e50c8c8a86a3640
KeesV <kverhaar@gmail.com>
2015-10-21 14:19:27 +0200

    First bit of integration testing

---
+++ IntegrationTests.Core/Helpers/FieldValuePair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests.Core.Helpers
+{
+    public class FieldValuePair
+    {
+        public string FieldReferenceName { get; set; }
+        public string FieldValue { get; set; }
+    }
+}
---
+++ IntegrationTests.Core/Helpers/TfsConnection.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests.Core.Helpers
+{
+    public class TfsConnection
+    {
+        private TfsTeamProjectCollection Tfs { get; set; }
+        private WorkItemStore _workItemStore { get; set; }
+
+        /// <summary>
+        /// Opens a connection to TFS and creates the necessary services to interact with it
+        /// </summary>
+        /// <param name="tpcUrl">The URL of the TPC to connect to</param>
+        /// <param name="userName">The username to use</param>
+        /// <param name="password">The password to use</param>
+        /// <param name="domain">The domain to use</param>
+        public TfsConnection(string tpcUrl, string userName, string password, string domain)
+        {
+            NetworkCredential nc = new NetworkCredential(userName, password, domain);
+
+            try
+            {
+                Tfs = new TfsTeamProjectCollection(new Uri(tpcUrl), nc);
+                Tfs.Authenticate();
+                Tfs.EnsureAuthenticated();
+
+                _workItemStore = new WorkItemStore(Tfs);
+            }
+            catch (WebException ex)
+            {
+                throw new ApplicationException(ex.Message, ex);
+            }
+        }
+
+        /// <summary>
+        /// Creates a work item in the specified project
+        /// </summary>
+        /// <param name="fieldValues">A list of field/value pairs to fill the new work item with</param>
+        /// <param name="workItemType">The work item type to create</param>
+        /// <param name="project">The project in which to create the work item</param>
+        /// <returns>The Id of the newly created work item</returns>
+        public int CreateWi(List<FieldValuePair> fieldValues, string workItemType, string project)
+        {
+
+            Project teamProject = _workItemStore.Projects[project];
+            WorkItemType wit = teamProject.WorkItemTypes[workItemType];
+
+            WorkItem wi = new WorkItem(wit);
+
+            foreach (FieldValuePair fv in fieldValues)
+            {
+                wi.Fields[fv.FieldReferenceName].Value = fv.FieldValue;
+            }
+
+            wi.Save();
+
+            return wi.Id;
+        }
+
+        /// <summary>
+        /// Destroys work items with the given Ids
+        /// </summary>
+        /// <param name="wisToDestroy">The Ids of the work items to destroy</param>
+        public void DestroyWis(List<int> wisToDestroy)
+        {
+            var errors = _workItemStore.DestroyWorkItems(wisToDestroy);
+            if (errors.Count > 0)
+            {
+                throw new ApplicationException("Failed to destroy all work items!");
+            }
+        }
+
+        /// <summary>
+        /// Destroys all Work Items in a specific project
+        /// </summary>
+        /// <param name="project">The project to destroy all Work Items in</param>
+        public void DestroyAllWisInProject(string project)
+        {
+            WorkItemCollection workItemCollection = _workItemStore.Query("SELECT [System.Id] FROM WorkItems WHERE [System.TeamProject] = '" + project +"'");
+
+            List<int> idsToDestroy = new List<int>();
+            foreach (WorkItem wi in workItemCollection)
+            {
+                idsToDestroy.Add(wi.Id);
+            }
+            DestroyWis(idsToDestroy);
+        }
+    }
+}
